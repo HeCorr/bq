a2e68f86eeb7a58fec79cdf31fcc0a0fac54996f
HeCorr <hccaseclone@gmail.com>
2024-02-19 01:44:38 -0300

    query_server_info now fetches player list
    remove query_server_players

--- src/api.rs
+++ src/api.rs
@@ -1,13 +1,18 @@
 mod models;
+mod util;
 
+use self::models::{Player, Server, ServerStatus};
 use anyhow::{anyhow, Result};
 use reqwest::StatusCode;
 use std::collections::HashMap;
 
-pub use self::models::{Player, Server, ServerStatus};
-
-pub fn query_server_info(server_id: u32) -> Result<Server> {
-    let url = format!("https://api.battlemetrics.com/servers/{server_id}");
+pub fn query_server_info(server_id: u32, include_online_players: bool) -> Result<Server> {
+    let url = format!(
+        "https://api.battlemetrics.com/servers/{server_id}{}",
+        include_online_players
+            .then_some("?include=player")
+            .unwrap_or("")
+    );
     let resp = reqwest::blocking::get(url)?;
 
     if resp.status() == StatusCode::NOT_FOUND {
@@ -15,6 +20,19 @@ pub fn query_server_info(server_id: u32) -> Result<Server> {
     }
 
     let data = resp.json::<HashMap<String, serde_json::Value>>()?;
+
+    let online_players = include_online_players
+        .then_some(
+            data["included"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .filter(|include| include["type"].as_str().unwrap() == "player")
+                .map(|player| player["attributes"]["name"].as_str().unwrap().to_owned())
+                .map(|name| Player { name })
+                .collect(),
+        )
+        .or(None);
 
     Ok(Server {
         name: data["data"]["attributes"]["name"]
@@ -62,27 +80,6 @@ pub fn query_server_info(server_id: u32) -> Result<Server> {
             .as_str()
             .unwrap_or("")
             .to_owned(),
+        online_players,
     })
-}
-
-pub fn query_server_players(server_id: u32) -> Result<Vec<Player>> {
-    let url = format!("https://api.battlemetrics.com/servers/{server_id}?include=player");
-    let resp = reqwest::blocking::get(url)?;
-
-    if resp.status() == StatusCode::NOT_FOUND {
-        return Err(anyhow!("Server not found."));
-    }
-
-    let data = resp.json::<HashMap<String, serde_json::Value>>()?;
-
-    let players = data["included"]
-        .as_array()
-        .unwrap()
-        .iter()
-        .filter(|include| include["type"].as_str().unwrap() == "player")
-        .map(|player| player["attributes"]["name"].as_str().unwrap().to_owned())
-        .map(|name| Player { name })
-        .collect();
-
-    Ok(players)
 }
